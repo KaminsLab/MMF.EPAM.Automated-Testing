ebda6713057e8cb45f012311fbb3936324f1f02a
KaminsLab <55655435+KaminsLab@users.noreply.github.com>
2021-11-21 11:01:05 +0300

    Initial refactor.

---
+++ CleenCode/Aircompany/Planes/PassengerPlane.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Aircompany.Planes
+{
+    public class PassengerPlane : Plane
+    {
+        public int _passengersCapacity;
+
+        public PassengerPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity)
+            :base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+        {
+            if (passengersCapacity > this.MaxLoadCapacity)
+            {
+                throw new ArgumentException("Passengers capacity is more than max load capacity.");
+            }
+            
+            _passengersCapacity = passengersCapacity;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var plane = obj as PassengerPlane;
+            return plane != null &&
+                   base.Equals(obj) &&
+                   _passengersCapacity == plane._passengersCapacity;
+        }
+
+        public override int GetHashCode() => (_passengersCapacity, base.GetHashCode()).GetHashCode();
+
+        public override string ToString()
+        {
+            return base.ToString().Replace("}",
+                    ", passengersCapacity=" + _passengersCapacity +
+                    '}');
+        }       
+        
+    }
+}
